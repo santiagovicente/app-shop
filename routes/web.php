4f1232ed541695f7ee66e95d27d3bb4fe7227205
santiago vicente <santiagovicente1989@gmail.com>
2017-12-04 16:45:59 +0100

    Base 2

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,7 @@
 
 Route::get('/', 'TestController@welcome');
 
-Route::get('prueba',function(){
-	return 'Hola soy una ruta de prueba';
-});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
